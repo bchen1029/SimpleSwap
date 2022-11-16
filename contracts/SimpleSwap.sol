50c8f4660472891325f07e8b59b8cc9527a3ca91
shih_gang <bchen1029@gmail.com>
2022-11-17 01:42:23 +0800

    chore: pass compile

--- contracts/SimpleSwap.sol
+++ contracts/SimpleSwap.sol
@@ -5,5 +5,40 @@ import { ISimpleSwap } from "./interface/ISimpleSwap.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SimpleSwap is ISimpleSwap, ERC20 {
-    // Implement core logic here
+    constructor() ERC20("aa", "bb") {}
+
+    function swap(
+        address tokenIn,
+        address tokenOut,
+        uint256 amountIn
+    ) external returns (uint256 amountOut) {
+        return 123;
+    }
+
+    function addLiquidity(uint256 amountAIn, uint256 amountBIn)
+        external
+        returns (
+            uint256 amountA,
+            uint256 amountB,
+            uint256 liquidity
+        )
+    {
+        return (1, 23, 4);
+    }
+
+    function removeLiquidity(uint256 liquidity) external returns (uint256 amountA, uint256 amountB) {
+        return (12, 3);
+    }
+
+    function getReserves() external view returns (uint256 reserveA, uint256 reserveB) {
+        return (1, 23);
+    }
+
+    function getTokenA() external view returns (address tokenA) {
+        return address(this);
+    }
+
+    function getTokenB() external view returns (address tokenB) {
+        return address(this);
+    }
 }
